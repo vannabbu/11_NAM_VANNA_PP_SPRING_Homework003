297eb2906aa6688b5b707f25c27d2795a145ff85
NamVanna <namvanna62@gmail.com>
2026-03-29 14:17:34 +0700

    first commit

---
+++ src/main/java/com/vannanamkh/springhomework3/SpringHomework3Application.java
@@ -0,0 +1,13 @@
+package com.vannanamkh.springhomework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomework3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomework3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/vannanamkh/springhomework3/SpringHomework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vannanamkh.springhomework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomework3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
